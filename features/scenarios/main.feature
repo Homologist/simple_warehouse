27b50638e7f16a73115cfd7b974db3eb7688a60d
Nzakou Cédric <nzakou.cedric@protonmail.com>
2018-06-25 22:42:39 +0200

    add cucumber scenarios

---
+++ features/scenarios/main.feature
@@ -0,0 +1,17 @@
+Feature: Move stuff in our storage
+  I want to move stuff in my storage
+  I see the input in the console
+
+  Scenario: I want move a crate
+    Given a grid
+    And a square crate of size 1 product code 42 at position 0 0
+    When I move the crate to position 3 2
+    Then I can see the crate at position 3 2
+
+  Scenario: I want locate all crates
+    Given a grid
+    And a square crate of size 1 product code 42 at position 0 0
+    And a square crate of size 1 product code 42 at position 1 4
+    And a square crate of size 2 product code 43 at position 3 1
+    When I locate crate of product code 42
+    Then I can see all crate of product code 42
---
+++ features/step_definitions/main_steps.rb
@@ -0,0 +1,23 @@
+Given("a grid") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("a square crate of size {int} product code {int} at position {int} {int}") do |int, int2, int3, int4|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I move the crate to position {int} {int}") do |int, int2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I can see the crate at position {int} {int}") do |int, int2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I locate crate of product code {int}") do |int|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I can see all crate of product code {int}") do |int|
+  pending # Write code here that turns the phrase above into concrete actions
+end
